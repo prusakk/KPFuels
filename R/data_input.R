c9d5bea2237d8bda182625ed257215839b964a53
Krzysztof Prusak <prusakk@student.agh.edu.pl>
2020-12-08 19:52:18 +0100

    co ja tutaj robie

---
+++ R/data_input.R
@@ -0,0 +1,14 @@
+#' Dane wejściowe
+#'
+#' @format Ramka danych zawiera:
+#' \describe{
+#'  \item{Nat}{character, zawiera informacje o typie samochody np. Passenger Cars}
+#'  \item{Segment}{character, klasa samochodu np. mini}
+#'  \item{Paliwo}{character, rodzaj paliwa, np. petrol}
+#'  \item{Technologia}{character, technologia użyta w silniku np. GDI}
+#' }
+#' @examples
+#' \dontrun{
+#'  input
+#' }
+"input"
