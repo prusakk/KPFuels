5195cd711cdb98253a5b7fdd10ae51be3bad266b
Krzysztof Prusak <prusakk@student.agh.edu.pl>
2020-12-08 12:46:26 +0100

    napisanie pierwszej funkcji (emisja)

---
+++ R/emisja.R
@@ -0,0 +1,19 @@
+emisja <- function(dane, kategoria, euro, mode, substancja) {
+
+  out <- wskazniki %>%
+    filter(Category %in% kategoria) %>%
+    filter(Euro.Standard %in% euro) %>%
+    filter(Pollutant %in% substancja) %>%
+    filter(Mode %in% mode)
+
+  out <- inner_join(x = out, y = input, by = "Segment")
+
+  out <- out %>%
+    mutate(Emisja = Nat * ((Alpha * Procent ^ 2 + Beta * Procent + Gamma + (Delta/Procent))/
+                             (Epsilon * Procent ^ 2 + Zita * Procent + Hta) * (1-Reduction))
+    ) %>%
+    select(Category, Fuel, Euro.Standard, Technology, Pollutant, Mode, Segment, Nat, Emisja)
+
+  return(out)
+
+}
