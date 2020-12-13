777b09974c90087cf1b55e88797e561f53abc592
Krzysztof Prusak <prusakk@student.agh.edu.pl>
2020-12-13 16:31:46 +0100

    Stowrzenie pliku KPFuels.R

---
+++ R/KPFuels.R
@@ -0,0 +1,18 @@
+#' KPFuels: Pakiet służący do obliczasnia emisji
+#'
+#' Pakiet służy do obliczania emisji zanieczyszczeń generowanych przez pojazdy
+#' z silnikami spalinowymi i wyswietlania wyników na wykresach.
+#' Dane są udostępniane przez Europejską Agencję Środowiska - EEA
+#' \url{https://www.eea.europa.eu/}
+#'
+#' Pakiet zawiera dwie funkcje: emisja(), emisja_plot()
+#'
+#' \code{emisja()} Obliczanie emisji
+#'
+#' \code{emisja_plot()} generowanie wykresu
+#'
+#' @docType package
+#' @name KPFuels
+#' @import dplyr
+#' @import plotly
+NULL
